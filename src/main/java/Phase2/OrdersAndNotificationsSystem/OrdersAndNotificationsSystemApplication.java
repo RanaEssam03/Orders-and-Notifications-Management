bc20e25cfd3c30de76212ea591825959d09adf9d
Rana Essam <95047435+RanaEssam03@users.noreply.github.com>
2023-12-22 15:52:48 +0200

    adding spring project

---
+++ src/main/java/Phase2/OrdersAndNotificationsSystem/OrdersAndNotificationsSystemApplication.java
@@ -0,0 +1,13 @@
+package Phase2.OrdersAndNotificationsSystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrdersAndNotificationsSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrdersAndNotificationsSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/Phase2/OrdersAndNotificationsSystem/OrdersAndNotificationsSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package Phase2.OrdersAndNotificationsSystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrdersAndNotificationsSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
