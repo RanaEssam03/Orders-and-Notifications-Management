411c0b8903ab5cf64a99505a0617ce1d4bc75525
Rana Essam <95047435+RanaEssam03@users.noreply.github.com>
2023-12-28 18:18:53 +0200

    Update Account.java

--- src/main/java/Phase2/OrdersAndNotificationsSystem/models/Account.java
+++ src/main/java/Phase2/OrdersAndNotificationsSystem/models/Account.java
@@ -1,12 +1,15 @@
 package Phase2.OrdersAndNotificationsSystem.models;
 import com.fasterxml.jackson.annotation.JsonIgnore;
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Data;
 
 
 @Data
 public class Account  {
 
+    @Schema(example = "ranaessam")
     private String username;
+    @Schema(example = "123456")
     private String password;
     private String email;
     private String phoneNumber;
