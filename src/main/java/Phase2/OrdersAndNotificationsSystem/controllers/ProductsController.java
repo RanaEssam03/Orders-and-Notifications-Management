4623b82a1439cf52b17296bc99112898ee70fe7b
mariam1h <96283723+mariam1h@users.noreply.github.com>
2023-12-31 11:40:41 +0200

    add javadoc to controllers

--- src/main/java/Phase2/OrdersAndNotificationsSystem/controllers/ProductsController.java
+++ src/main/java/Phase2/OrdersAndNotificationsSystem/controllers/ProductsController.java
@@ -1,4 +1,5 @@
 package Phase2.OrdersAndNotificationsSystem.controllers;
+
 import Phase2.OrdersAndNotificationsSystem.models.Product;
 import Phase2.OrdersAndNotificationsSystem.models.exceptions.GeneralException;
 import Phase2.OrdersAndNotificationsSystem.services.products.ProductServices;
@@ -7,8 +8,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
-// run the server and go to http://localhost:8080/swagger-ui.html
 
+/**
+ * Controller class for managing product-related operations.
+ * Provides endpoints to retrieve product information.
+ */
 @RequestMapping("/api/products")
 @RestController
 public class ProductsController {
@@ -19,19 +23,28 @@ public class ProductsController {
     @Autowired
     JwtTokenUtil jwtTokenUtil;
 
-
+    /**
+     * Retrieves all products from the system.
+     *
+     * @param authHeader The authorization header containing the JWT token.
+     * @return An ArrayList of Product objects representing all products in the system.
+     * @throws GeneralException If there is an issue with the authorization token.
+     */
     @GetMapping("/get-all")
     public ArrayList<Product> getAllProducts(@RequestHeader("Authorization") String authHeader ) throws GeneralException {
-
-         jwtTokenUtil.getUsernameFromToken(authHeader.substring(7));
+        jwtTokenUtil.getUsernameFromToken(authHeader.substring(7));
         return productService.getAllProducts();
     }
 
+    /**
+     * Retrieves all products belonging to a specific category.
+     *
+     * @param id The ID of the category for which products are to be retrieved.
+     * @return An ArrayList of Product objects representing products in the specified category.
+     * @throws GeneralException If there is an issue with the retrieval of products by category.
+     */
     @GetMapping("/all-by-category/{id}")
     public ArrayList<Product> getProductsByCategory(@PathVariable("id") Integer id) throws GeneralException {
-
         return productService.getProductsByCategory(id);
     }
-
-
 }
